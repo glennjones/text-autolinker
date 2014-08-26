5996995ace363f612c664aadc56f64949243d1bc
Glenn Jones <glennjonesnet@gmail.com>
2014-08-26 10:21:39 +0100

    tomorrow and tonight date correction/tests

--- test/autolinker-date-test.js
+++ test/autolinker-date-test.js
@@ -67,7 +67,7 @@ describe('autolinker-date', function(){
 			text: 'An appointment on 9 May 2014 at 16:10',
 		};
 		autolinker.parse(options, function(err, result){
-			assert.equal(result.html, 'An appointment on <time class=\"auto-date-discovery\" datetime=\"2014-05-09T16:10\">9 May 2014 at 16:10</time>', 'should convert to html');
+			assert.equal(result.html, 'An appointment on <time class=\"auto-date-discovery\" datetime=\"2014-05-09T16:10:00\">9 May 2014 at 16:10</time>', 'should convert to html');
 			assert.equal(err, null, 'should excute without error');
 			done();
 		});
@@ -80,7 +80,7 @@ describe('autolinker-date', function(){
 			publishedDate: '2012-07-23T00:00+02:00'
 		};
 		autolinker.parse(options, function(err, result){
-			assert.equal(result.html, 'An appointment on <time class=\"auto-date-discovery\" datetime=\"2014-05-10T16:10+02:00\">10 May 2014 at 16:10</time>', 'should convert to html');
+			assert.equal(result.html, 'An appointment on <time class=\"auto-date-discovery\" datetime=\"2014-05-10T16:10:00+02:00\">10 May 2014 at 16:10</time>', 'should convert to html');
 			assert.equal(err, null, 'should excute without error');
 			done();
 		});
@@ -91,7 +91,7 @@ describe('autolinker-date', function(){
 			text: 'An appointment on 13 May 2014 at 16:10 EDT',
 		};
 		autolinker.parse(options, function(err, result){
-			assert.equal(result.html, 'An appointment on <time class=\"auto-date-discovery\" datetime=\"2014-05-13T16:10-04:00\">13 May 2014 at 16:10 EDT</time>', 'should convert to html');
+			assert.equal(result.html, 'An appointment on <time class=\"auto-date-discovery\" datetime=\"2014-05-13T16:10:00-04:00\">13 May 2014 at 16:10 EDT</time>', 'should convert to html');
 			assert.equal(err, null, 'should excute without error');
 			done();
 		});
@@ -149,6 +149,47 @@ describe('autolinker-date', function(){
 			done();
 		});
 	});
+
+
+	it('today with reference date ref set to 2012-01-01', function(done){
+		var options = {
+			text: 'today',
+			publishedDate: '2012-01-01'
+		};
+		autolinker.parse(options, function(err, result){
+			assert.equal(result.html, '<time class=\"auto-date-discovery\" datetime=\"2012-01-01\">today</time>', 'should convert to html');
+			assert.equal(err, null, 'should excute without error');
+			done();
+		});
+	});
+
+
+	it('tonight with reference date ref set to 2012-01-01', function(done){
+		var options = {
+			text: 'tonight',
+			publishedDate: '2012-01-01'
+		};
+		autolinker.parse(options, function(err, result){
+			assert.equal(result.html, '<time class=\"auto-date-discovery\" datetime=\"2012-01-01\">tonight</time>', 'should convert to html');
+			assert.equal(err, null, 'should excute without error');
+			done();
+		});
+	});
+
+
+	it('tomorrow at 4:15 with reference date ref set to 2012-01-01', function(done){
+		var options = {
+			text: 'tomorrow at 4:15',
+			publishedDate: '2012-01-01'
+		};
+		autolinker.parse(options, function(err, result){
+			assert.equal(result.html, '<time class=\"auto-date-discovery\" datetime=\"2012-01-02T04:15:00\">tomorrow at 4:15</time>', 'should convert to html');
+			assert.equal(err, null, 'should excute without error');
+			done();
+		});
+	});
+
+
 
 
 	
